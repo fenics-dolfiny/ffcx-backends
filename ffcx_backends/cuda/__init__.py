e9a03665387090ec77753a3d7c69f518876da1ef
Paul T. Kühner <56360279+schnellerhase@users.noreply.github.com>
2026-03-26 22:30:30 +0000

    Move jit to cuda/

--- ffcx_backends/cuda/__init__.py
+++ ffcx_backends/cuda/__init__.py
@@ -1,9 +1,11 @@
 """Custom CUDA FFCx backend."""
 
 from ffcx_backends.cuda import expression, file, form, integral
-from ffcx_backends.cuda.jit import compile_objects
+from ffcx_backends.cuda.jit import compile_expressions, compile_forms
 
 __all__ = [
+    "compile_expressions",
+    "compile_forms",
     "compile_objects",
     "expression",
     "file",
